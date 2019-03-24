a18cc791400dcfbca78a439edf60e87708422e96
Prototype <263693992@qq.com>
2019-03-24 21:34:30 +0800

    [mod] enable checkout submodules

---
+++ install.sh
@@ -0,0 +1,8 @@
+source install_3rdparty.sh
+
+cd $libonvif_dir
+rm -r build
+mkdir build
+cd build
+cmake ..
+make -j8 & make install
--- install_3rdparty.sh
+++ install_3rdparty.sh
@@ -1,23 +1,20 @@
 # /bin/sh
 
 libonvif_dir=$(pwd)
+rm -r lib
+mkdir lib
+
+git submodule update
 
 ## build 3rdparty libs
-# glog
+glog
 cd ./3rdparty/glog
 rm -r build
 mkdir build
 cd ./build
-echo $libonvif_dir/modlules/common/glog
+# echo $libonvif_dir/modlules/common/glog
 cmake .. -DBUILD_SHARED_LIBS=ON
 make -j8
-cp -r ./glog $libonvif_dir/modules/common/glog
+cp -r ./glog $libonvif_dir/modules/common
 cp ../src/glog/log_severity.h $libonvif_dir/modules/common/glog
-cp libglog.so $libonvif_dir/lib
-
-# cd $libonvif_dir
-# rm -r build
-# mkdir build
-# cd build
-# cmake ..
-# make -j8 & make install
+cp libglog.so $libonvif_dir/lib;
